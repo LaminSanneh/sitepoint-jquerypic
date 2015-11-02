ca99be27bd38b29e3bdbedcc58bff61811afd082
Lamin Sanneh <lamin.evra@gmail.com>
2015-11-02 09:29:51 +0000

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	$(".jquerypic").jquerypic();
+});
---
+++ js/jquerypic.js
@@ -0,0 +1,34 @@
+(function($) {
+	var Jquerypic = function (element, options) {
+		
+		// Setting default settings
+		var settings = $.extend({
+			thumbnailsContainer: ".thumbnails",
+			thumbnail: ".thumbnail",
+			fullversionContainer: ".fullversion-container",
+			fullVersion: ".full-version"
+		}, options),
+		plugin = this;
+		
+		// Storing settings as a property in plugin
+		this.settings = settings;
+		this.element = element;
+		
+		return element.each(function(){
+			plugin.initializePlugin(element);
+		});
+	};
+	
+	Jquerypic.prototype.initializePlugin = function (element) {
+		var fullVersion = element.find(this.settings.fullVersion);
+		
+		element.on("click", this.settings.thumbnail, function(){
+			var src = $(this).attr("src");
+			fullVersion.attr("src", src);
+		});
+	}
+	
+	$.fn.jquerypic = function (options) {
+		instance = new Jquerypic(this, Jquerypic);
+	}; 
+})(jQuery)
